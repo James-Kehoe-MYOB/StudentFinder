080ab4f0358a1bb613f6b8c56cc099b3418ec56f
James-Kehoe-MYOB <james.kehoe@myob.com>
2020-02-27 16:17:19 +1100

    Initialise Repository

---
+++ StudentFinder/Program.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace StudentFinder {
+    class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ StudentFinder_Tests/UnitTest1.cs
@@ -0,0 +1,10 @@
+using System;
+using Xunit;
+
+namespace StudentFinder_Tests {
+    public class UnitTest1 {
+        [Fact]
+        public void Test1() {
+        }
+    }
+}
